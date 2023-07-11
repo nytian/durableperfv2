825fe9206e7a6ba85270febc18b9e1b211a332ea
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2023-07-10 18:29:16 -0700

    first commit

---
+++ DFPerfScenarios/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace DFPerfScenarios.DFPerfScenarios
+{
+    public class HelloWorld
+    {
+        [FunctionName("HelloWorldOrchestrator")]
+        public string HelloWorldOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext ctx)
+        {
+            var input = ctx.GetInput<dynamic>();
+            return $"{input.Hello}, {input.Name}";
+        }
+
+        [FunctionName("FailedActivity")]
+        public string FailedActivity(
+    [ActivityTrigger] IDurableActivityContext ctx)
+        {
+            return null;
+        }
+    }
+}
